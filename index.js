9f22eff02f1d30880ff9466583daa7d1750248b4
lucasgfsouza <lucasgfsouza@outlook.com>
2021-01-10 10:28:31 -0300

    project conclude, uploaded in one round

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const session = require("express-session");
+
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/articlesController");
+const usersController = require("./users/UsersController");
+
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
+const User = require("./users/User")
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: "qualquercoisa",
+    cookie: {maxAge: 300000},
+}))
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+connection
+    .authenticate()
+    .then(()=> {
+        console.log("sucesso");
+    }).catch((error)=> {
+        console.log(error);
+    });
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
+app.use("/", usersController);
+
+app.get("/", (req, res)=> {
+
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ],
+        limit: 4
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});    
+        })
+        
+    });
+    
+});
+
+app.get("/:slug", (req,res)=> {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+              Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+              });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    })
+})
+
+app.get("/category/:slug", (req,res)=> {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then( category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories})
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
+})
+
+app.listen(8080, ()=> {
+    console.log("o servidor está rodando")
+});
